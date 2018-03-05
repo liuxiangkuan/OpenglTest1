fabc06410f545b60c6966eab9bd87b5ec0e29865
刘祥宽 <36977539+liuxiangkuan@users.noreply.github.com>
2018-03-05 10:07:48 +0800

    添加注释

--- OPenglTest1/OPenglTest1/OpenglTest1.cpp
+++ OPenglTest1/OPenglTest1/OpenglTest1.cpp
@@ -1,26 +1,35 @@
 #include"GL\glut.h"
 #include<iostream>
 
+//方块位置
 GLfloat ix = 0.0f;
 GLfloat iy = 0.0f;
+
+//方块尺寸
 GLfloat csize = 25;
 
+//x移动步数
 GLfloat xstep = 1.0f;
+//y移动步数
 GLfloat ystep = 1.0f;
 
+//窗口宽度
 GLfloat WindowHeight;
+//窗口高度
 GLfloat WindowWidth;
 
 
 void Rendersence(void)
 {	//窗口清除
 	glClear(GL_COLOR_BUFFER_BIT);
+	//设置方块颜色
 	glColor3f(1.0f, 0.0f, 0.0f);
 	glRectf(ix, iy, ix + csize, iy - csize);
 	//刷新绘图命令
 	glutSwapBuffers();
 }
 
+//移动逻辑控制函数相当于DX的switch循环
 void TimerFunction(int value)
 {
 	if (ix>WindowWidth - csize || ix<-WindowWidth)
@@ -50,6 +59,7 @@ void TimerFunction(int value)
 	{
 		iy = -WindowHeight + csize - 1;
 	}
+	//刷新窗口
 	glutPostRedisplay();
 	glutTimerFunc(33, TimerFunction, 1);
 }
@@ -61,13 +71,14 @@ void setUpRC(void)
 void changeSize(GLsizei w, GLsizei h)
 {
 	GLfloat a;
+	//防止除数为0
 	if (h == 0)
 	{
 		h = 1;
 	}
-	glViewport(0, 0, w, h);
-	//重置坐标位置
+	glViewport(0, 0, w, h);	
 	glMatrixMode(GL_PROJECTION);
+	//重置坐标位置
 	glLoadIdentity();
 
 	a = (GLfloat)w / (GLfloat)h;
@@ -75,6 +86,7 @@ void changeSize(GLsizei w, GLsizei h)
 	{
 		WindowWidth = 100;
 		WindowHeight = 100 / a;
+		//设置裁剪空间大小
 		glOrtho(-100.0, 100.0, -100.0 / a, 100.0 / a, 1.0, -1.0);
 	}
 	else
@@ -97,6 +109,7 @@ int main(int argc, char *argv[])
 	glutInitWindowSize(800, 600);
 	glutCreateWindow("first");
 	glutDisplayFunc(Rendersence);
+	//调用重新渲染函数
 	glutReshapeFunc(changeSize);
 	glutTimerFunc(33, TimerFunction, 1);
 	setUpRC();
